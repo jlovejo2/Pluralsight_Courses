8b6aa3c6771dc0971a4c181991914b1bdf860e90
James Lovejoy <james.lovejoy2@gmail.com>
2020-10-30 12:14:07 -0500

    created a constants class to provide gets for bank and routing numbers, that is then used in bank_account

--- typescript_object_oriented/src/scripts/bank-account.ts
+++ typescript_object_oriented/src/scripts/bank-account.ts
@@ -1,5 +1,6 @@
 import { Account, AccountInfo, AccountSettings } from './interfaces';
 import { AccountType } from './enums';
+import { Constants } from "./constants";
 
 export abstract class BankAccount implements Account {
     private _balance: number = 0;
@@ -25,7 +26,12 @@ export abstract class BankAccount implements Account {
     }
     } 
 
-    abstract getAccountInfo(): AccountInfo<number, number>
+    getAccountInfo(): AccountInfo<string, number> {
+        return {
+            routingNumber: Constants.ROUTING_NUMBER,
+            bankNumber: Constants.BANK_NUMBER 
+        }
+    }
 
     deposit(amount: number) {
         this._balance += amount;
