a63a0120a196c64c29ee9341a27684a470668a6f
James Lovejoy <james.lovejoy2@gmail.com>
2020-10-30 11:58:36 -0500

    made more interfaces using generics and extensions of other interfaces

--- typescript_object_oriented/src/scripts/interfaces.ts
+++ typescript_object_oriented/src/scripts/interfaces.ts
@@ -1,9 +1,20 @@
-export interface AccountInfo {
-    routingNumber: number;
-    bankNumber: number;
+export interface AccountInfo<TRouteNumber, TBankNumber> {
+    routingNumber: TRouteNumber;
+    bankNumber: TBankNumber;
 }
 
 export interface DepositWithdrawal {
     deposit(amount: number): void;
     withdrawal(amount: number): void;
 }
+
+export interface AccountSettings {
+    id: number;
+    title: number;
+    balance: number;
+    interestRate?: number;
+    accountInfo?: AccountInfo<string ,number>
+}
+
+//to have an account you have to have deposit(), withdrawal(), id, title, and balance to make an account
+export interface Account extends AccountSettings, DepositWithdrawal {}
