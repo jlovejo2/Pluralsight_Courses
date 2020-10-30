7e8c0200adf21364645ba9764a3dbe6fdd9d9c53
James Lovejoy <james.lovejoy2@gmail.com>
2020-10-30 07:33:32 -0500

    initial commit ... again

---
+++ typescript_object_oriented/src/scripts/checking-account.ts
@@ -0,0 +1,22 @@
+﻿export class CheckingAccount {
+    private _balance: number = 0;
+
+    constructor(public title: string) { }
+
+    get balance() {
+        return this._balance
+    }
+
+    set balance(value:number) {
+        this._balance = value;
+    }
+
+    deposit(amount: number) {
+        this._balance += amount;
+    }
+
+    withdrawal(amount: number) {
+        this.balance -= amount;
+    }
+}
+
