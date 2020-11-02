b5ed1181a2ace83ccd04be7abef784df4839ac47
James Lovejoy <james.lovejoy2@gmail.com>
2020-11-02 07:23:25 -0600

    created hero.component.ts

---
+++ asynchronous_typescript/src/heroes.component.ts
@@ -0,0 +1,34 @@
+import { Hero, createDiv, cloneElementsFromTemplate, setText } from './lib';
+
+export function replaceHeroListComponent(hero?: Hero) {
+    const heroPlaceHolder = document.querySelector('.hero-list');
+    const el = hero ? createList() : createNoneFound();
+
+    heroPlaceHolder.replaceWith(el);
+
+    function createList() {
+        const ul = document.createElement('ul');
+        ul.classList.add('list', 'hero-list');
+        ul.appendChild(createHeroCardFromTemplate(hero));
+        return ul;
+    }
+
+    function createNoneFound() {
+        const div = createDiv('hero-list');
+        div.innerText = 'No heroes found';
+        return div;
+    }
+}
+
+//________________________________________________________
+// ------------Private Functions to this module-----------
+//_________________________________________________________
+
+function createHeroCardFromTemplate(hero: Hero) {
+    const heroClone = cloneElementsFromTemplate('hero-template');
+    setText(heroClone, '.description', hero.description);
+    setText(heroClone, '.name', hero.name);
+    setText(heroClone, '.emal', hero.email);
+    heroClone.querySelector('.card').classList.add(hero.name);
+    return heroClone;
+}
