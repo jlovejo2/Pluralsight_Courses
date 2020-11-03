5fa776062c4fa180b33950ad9ec6a719e3fdc293
James Lovejoy <james.lovejoy2@gmail.com>
2020-11-03 05:59:33 -0600

    fixed syntax for modal.ts and timer.ts

--- asynchronous_typescript/src/lib/modal.ts
+++ asynchronous_typescript/src/lib/modal.ts
@@ -1,35 +1,34 @@
 export const openModal = async function() {
-    const listenerYes = () => closeModal('yes');
-    const listenerNo = () => closeModal('no');
-    document.querySelector('.modal-yes').addEventListener('click', listenerYes);
-    document.querySelector('.modal-no').addEventListener('click', listenerNo);
+  const listenerYes = () => closeModal('yes');
+  const listenerNo = () => closeModal('no');
+  document.querySelector('.modal-yes').addEventListener('click', listenerYes);
+  document.querySelector('.modal-no').addEventListener('click', listenerNo);
 
-const modalWindow = document.getElementById('modal');
+  const modalWindow = document.getElementById('modal');
 
-if ( modalWindow.classList) {
+  if (modalWindow.classList) {
     modalWindow.classList.add('is-active');
-}
+  }
 
-let resolve: (value?: string) => void;
-const responsePromise = new Promise<string>((res, rej) => {
+  let resolve: (value?: string) => void;
+  const responsePromise = new Promise<string>((res, rej) => {
     resolve = res;
-})
-//This line returns the promise, in turn the caller then waits for it to resolve
-return responsePromise;
+  });
+  //This line returns the promise, in turn the caller then waits for it to resolve
+  return responsePromise;
 
-function closeModal(yesno: 'yes' | 'no') {
-
-    if(modalWindow.classList) {
-        modalWindow.classList.remove('is-active')
+  function closeModal(yesno: 'yes' | 'no') {
+    if (modalWindow.classList) {
+      modalWindow.classList.remove('is-active');
     }
 
-    modalWindow.querySelector('.modal-yes').removeEventListener('click', listenerYes);
-    modalWindow.querySelector('.modal-no').removeEventListener('click', listenerNo);
-    
+    modalWindow
+      .querySelector('.modal-yes')
+      .removeEventListener('click', listenerYes);
+    modalWindow
+      .querySelector('.modal-no')
+      .removeEventListener('click', listenerNo);
+
     return resolve(yesno);
-
-}
-
-
-
-}
+  }
+};
--- asynchronous_typescript/src/lib/timer.ts
+++ asynchronous_typescript/src/lib/timer.ts
@@ -1,25 +1,25 @@
 import { showMessage } from './dom';
 
 export const sayHelloTimer = function(ms: number) {
-    let counter = 0;
-    showMessage('Starting the timer', 'Response from Timer');
+  let counter = 0;
+  showMessage('Starting the timer', 'Response from Timer');
 
-    const callBack = (ms: number) => {
-        counter++;
-        showMessage(
-            `Hello every ${ms} milliseconds. (${counter} iterations)`,
-            'Response from Timer',
-            true,
-        );
+  const callBack = (ms: number) => {
+    counter++;
+    showMessage(
+      `Hello every ${ms} milliseconds. (${counter} iterations)`,
+      'Response from Timer',
+      true,
+    );
 
-        if ( counter === 5 ) {
-            showMessage(
-                `Goodbye. We said hello every ${ms} milliseconds. (after ${counter} iterations)`,
-                'Response from Timer',
-                true,
-            );
-            clearInterval(intervalId);
-        }
+    if (counter === 5) {
+      showMessage(
+        `Goodbye. We said hello every ${ms} milliseconds. (after ${counter} iterations)`,
+        'Response from Timer',
+        true,
+      );
+      clearInterval(intervalId);
     }
-    const intervalId = setInterval(callBack, ms, ms);
-}
+  };
+  const intervalId = setInterval(callBack, ms, ms);
+};
