b8a6bcee84c14db8e1365dd6146a90576f3fe329
James Lovejoy <james.lovejoy2@gmail.com>
2020-11-03 06:48:57 -0600

    updated index.ts and heroes.component to match activity 4,  added code to grab the ingredients location in index.html and fetch data

--- asynchronous_typescript/src/heroes.component.ts
+++ asynchronous_typescript/src/heroes.component.ts
@@ -1,23 +1,23 @@
 import { Hero, createDiv, cloneElementsFromTemplate, setText } from './lib';
 
 export function replaceHeroListComponent(hero?: Hero) {
-    const heroPlaceHolder = document.querySelector('.hero-list');
-    const el = hero ? createList() : createNoneFound();
+  const heroPlaceHolder = document.querySelector('.hero-list');
+  const el = hero ? createList() : createNoneFound();
 
-    heroPlaceHolder.replaceWith(el);
+  heroPlaceHolder.replaceWith(el);
 
-    function createList() {
-        const ul = document.createElement('ul');
-        ul.classList.add('list', 'hero-list');
-        ul.appendChild(createHeroCardFromTemplate(hero));
-        return ul;
-    }
+  function createList() {
+    const ul = document.createElement('ul');
+    ul.classList.add('list', 'hero-list');
+    ul.appendChild(createHeroCardFromTemplate(hero));
+    return ul;
+  }
 
-    function createNoneFound() {
-        const div = createDiv('hero-list');
-        div.innerText = 'No heroes found';
-        return div;
-    }
+  function createNoneFound() {
+    const div = createDiv('hero-list');
+    div.innerText = 'No heroes found';
+    return div;
+  }
 }
 
 //________________________________________________________
@@ -25,10 +25,10 @@ export function replaceHeroListComponent(hero?: Hero) {
 //_________________________________________________________
 
 function createHeroCardFromTemplate(hero: Hero) {
-    const heroClone = cloneElementsFromTemplate('hero-template');
-    setText(heroClone, '.description', hero.description);
-    setText(heroClone, '.name', hero.name);
-    setText(heroClone, '.email', hero.email);
-    heroClone.querySelector('.card').classList.add(hero.name);
-    return heroClone;
+  const heroClone = cloneElementsFromTemplate('hero-template');
+  setText(heroClone, '.description', hero.description);
+  setText(heroClone, '.name', hero.name);
+  setText(heroClone, '.email', hero.email);
+  heroClone.querySelector('.card').classList.add(hero.name);
+  return heroClone;
 }
--- asynchronous_typescript/src/index.ts
+++ asynchronous_typescript/src/index.ts
@@ -1,40 +1,61 @@
 import './design/index.scss';
 
-import { Hero, getHeroAsync, openModal, sayHelloTimer, showFetching, showMessage } from './lib';
+import {
+  Hero,
+  getHeroAsync,
+  showFetching,
+  showMessage,
+  openModal,
+  sayHelloTimer,
+} from './lib';
 import { replaceHeroListComponent } from './heroes.component';
+import { getDataAfterDelay } from './examples/get-ingredients';
 
-const searchEmailElement = document.getElementById('search-email') as HTMLInputElement;
-const button = document.querySelector('.search-button')
+const searchEmailElement = document.getElementById(
+  'search-email',
+) as HTMLInputElement;
+const button = document.querySelector('.search-button');
 searchEmailElement.addEventListener('keydown', (e: KeyboardEvent) => {
-  if( e.code === 'Enter') render(); 
-})
+  if (e.code === 'Enter') render();
+});
 button.addEventListener('click', render);
 
-document.querySelector('#open-modal').addEventListener('click', async() => {
+document.querySelector('#open-modal').addEventListener('click', async () => {
   openModal().then((resp: string) => {
-    const msg = 
-    resp === 'yes'  
-    ? `Yay! This is fun! 😄`
-    : `Aw, that is sad. Let's try harder to have fun 😞`;
+    const msg =
+      resp === 'yes'
+        ? `Yay! This is fun! 😄`
+        : `Aw, that is sad. Let's try harder to have fun 😞`;
 
     showMessage(msg, 'Response from Modal');
-  })
+  });
 });
 
-document.querySelector('#run-timer').addEventListener('click', async() => {
+document.querySelector('#run-timer').addEventListener('click', async () => {
   sayHelloTimer(10000);
-})
+});
+
+document
+  .querySelector('#show-ingredients')
+  .addEventListener('click', getIngredients);
+
+function getIngredients() {
+  showMessage('Ingredients for baking amazing cookies:', 'Ingredients');
+  getDataAfterDelay(1500, function(ingredients: string[]) {
+    ingredients.forEach(i => showMessage(`${i}`, 'Ingredients', true));
+  });
+}
 
 async function render() {
-  showMessage()
+  showMessage();
   showFetching('.hero-list');
   let hero: Hero;
   try {
     hero = await getHeroAsync(searchEmailElement.value);
-  } catch(err) {
-    console.error(err)
-    showMessage(err)
+  } catch (err) {
+    console.error(err);
+    showMessage(err);
   } finally {
-    replaceHeroListComponent(hero)
+    replaceHeroListComponent(hero);
   }
 }
